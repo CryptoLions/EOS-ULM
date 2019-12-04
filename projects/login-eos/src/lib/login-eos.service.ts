e21d0e9ff626188cc3b9becb3a7f136dfc54c71a
orange1337 <rosman12@gmail.com>
2019-12-04 11:10:03 +0200

    revert

--- projects/login-eos/src/lib/login-eos.service.ts
+++ projects/login-eos/src/lib/login-eos.service.ts
@@ -39,6 +39,7 @@ export class LoginEOSService {
   initCounterErr = 0;
   eosTock: any;
   inProgress = false;
+  accountInfo: any;
 
   constructor(private toastyService: ToastaService,
               private toastyConfig: ToastaConfig,
@@ -69,6 +70,7 @@ export class LoginEOSService {
 
               let account = ScatterJS.account('eos'); 
               this.accountName = account.name;
+              this.accountInfo = account;
               this.options = {authorization:[`${account.name}@${account.authority}`]};
 
               localStorage.setItem('walletConnected', 'connected');
@@ -116,6 +118,9 @@ export class LoginEOSService {
       }).catch(error => {
           this.showScatterError(error);
       });
+  }
+  initLYNX(data){
+      this.WINDOW.location.href = "https://magic.lynxwallet.io/8epm";
   }
 
   /*initLedger() {
